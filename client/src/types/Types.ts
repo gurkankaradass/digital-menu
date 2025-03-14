eee62816ec85eabed895b458e48004c8fcc3cdb8
Gürkan Karadaş <gurkan.karadas@outlook.com.tr>
2025-03-15 00:46:14 +0300

    Type definitions have been made.

---
+++ client/src/types/Types.ts
@@ -0,0 +1,46 @@
+export interface CafeInfoType {
+    id: number,
+    name: string,
+    logo: string,
+    phone: string,
+    location: string,
+    address: string,
+    map: string,
+    instagram: string
+}
+
+export interface EmployeeType {
+    id: number,
+    username: string,
+    password: string,
+    role: string
+}
+
+export interface CategoryType {
+    id: number,
+    name: string,
+    image: string
+}
+
+export interface ProductType {
+    id: number,
+    name: string,
+    image: string,
+    price: number,
+    categoryName: string
+}
+
+export interface TableType {
+    id: number,
+    tableNumber: number,
+    bill: number
+}
+
+export interface OrderType {
+    id: number,
+    tableNumber: number,
+    productName: string,
+    productImage: string,
+    quantity: number,
+    totalPrice: number
+}
