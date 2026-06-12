9c3ae43097dfead7c782be5891c9ae04f5db4756
Gürkan Karadaş <gurkan.karadas@outlook.com.tr>
2026-06-13 01:08:52 +0300

    Configured axios instance with dynamic base URL support.

--- client/src/config/AxiosConfig.ts
+++ client/src/config/AxiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: `http://${window.location.hostname}:5000/`
+    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? `http://${window.location.hostname}:5000/` : "https://digital-menu-backend-gzqe.onrender.com/")
 });
 
 export default axiosInstance;
